2715e7cf80f9fbd7e6c19b5179a5272d82491b19
ThikerXCX <rifkimuhammad967@gmail.com>
2021-11-23 15:23:33 +0700

    minggu 10

--- src/app/service/scheduleData.ts
+++ src/app/service/scheduleData.ts
@@ -1,32 +1,67 @@
-import { Schedule } from "../interface/schedule";
-import { Team } from "../interface/team";
+import { Schedule } from '../interface/schedule';
+import { Team } from '../interface/team';
 
-export const SEASON_SCHEDULE:Schedule[]=[
-    {id:1,PlayingDate:new Date(2021,08,10),HomeTeam:'persib',
-    AwayTeam:'persija',HomeScore:5,AwayScore:6,
-    RefName:'udin',notes:'overtime'},
-    {id:2,PlayingDate:new Date(2021,08,11),HomeTeam:'Tepos',
-    AwayTeam:'BadutMPL',HomeScore:5,AwayScore:6,
-    RefName:'KB',notes:'badut ngelawak'},
-    {id:3,PlayingDate:new Date(2021,08,15),HomeTeam:'blackgay',
-    AwayTeam:'onic',HomeScore:5,AwayScore:6,
-    RefName:'rijen',notes:'apakah ohmyvenus wanita?'},
-    {id:4,PlayingDate:new Date(2021,08,14),HomeTeam:'BTR',
-    AwayTeam:'Luminairesibadut',HomeScore:5,AwayScore:6,
-    RefName:'ericolim',notes:'mereka memanggilku pahlawan'},
-    {id:5,PlayingDate:new Date(2021,08,18),HomeTeam:'setang',
-    AwayTeam:'bagol',HomeScore:5,AwayScore:6,
-    RefName:'marsha ozawa',notes:'apa itu setan'},
-]
-export const Teams:Team[]=[
-    {id:1,name:'persija',type:'local'},
-    {id:2,name:'persib',type:'local'},
-    {id:3,name:'Tepos',type:'local'},
-    {id:4,name:'BadutMPL',type:'local'},
-    {id:4,name:'blackgay',type:'internasional'},
-    {id:5,name:'onic',type:'local'},
-    {id:6,name:'BTR',type:'local'},
-    {id:7,name:'Luminairesibadut',type:'local'},
-    {id:8,name:'setang',type:'local'},
-    {id:9,name:'bagol',type:'local'},
-]
+export const SEASON_SCHEDULE: Schedule[] = [
+  {
+    id: 1,
+    PlayingDate: new Date(2021, 8, 10),
+    HomeTeam: 'persib',
+    AwayTeam: 'persija',
+    HomeScore: 5,
+    AwayScore: 6,
+    RefName: 'udin',
+    notes: 'overtime',
+  },
+  {
+    id: 2,
+    PlayingDate: new Date(2021, 8, 11),
+    HomeTeam: 'Tepos',
+    AwayTeam: 'BadutMPL',
+    HomeScore: 5,
+    AwayScore: 6,
+    RefName: 'KB',
+    notes: 'badut ngelawak',
+  },
+  {
+    id: 3,
+    PlayingDate: new Date(2021, 8, 15),
+    HomeTeam: 'blackgay',
+    AwayTeam: 'onic',
+    HomeScore: 5,
+    AwayScore: 6,
+    RefName: 'rijen',
+    notes: 'apakah ohmyvenus wanita?',
+  },
+  {
+    id: 4,
+    PlayingDate: new Date(2021, 8, 14),
+    HomeTeam: 'BTR',
+    AwayTeam: 'Luminairesibadut',
+    HomeScore: 5,
+    AwayScore: 6,
+    RefName: 'ericolim',
+    notes: 'mereka memanggilku pahlawan',
+  },
+  {
+    id: 5,
+    PlayingDate: new Date(2021, 8, 18),
+    HomeTeam: 'setang',
+    AwayTeam: 'bagol',
+    HomeScore: 5,
+    AwayScore: 6,
+    RefName: 'marsha ozawa',
+    notes: 'apa itu setan',
+  },
+];
+export const Teams: Team[] = [
+  { id: 1, name: 'persija', type: 'local' },
+  { id: 2, name: 'persib', type: 'local' },
+  { id: 3, name: 'Tepos', type: 'local' },
+  { id: 4, name: 'BadutMPL', type: 'local' },
+  { id: 4, name: 'blackgay', type: 'internasional' },
+  { id: 5, name: 'onic', type: 'local' },
+  { id: 6, name: 'BTR', type: 'local' },
+  { id: 7, name: 'Luminairesibadut', type: 'local' },
+  { id: 8, name: 'setang', type: 'local' },
+  { id: 9, name: 'bagol', type: 'local' },
+];
--- src/app/standing/standing.component.ts
+++ src/app/standing/standing.component.ts
@@ -1,15 +1,105 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { scheduled } from 'rxjs';
+import { Rangking } from '../interface/rangking';
+import { Schedule } from '../interface/schedule';
+import { Team } from '../interface/team';
+import { SoccerService } from '../service/Soccer.Service';
 
 @Component({
   selector: 'app-standing',
   templateUrl: './standing.component.html',
-  styleUrls: ['./standing.component.css']
+  styleUrls: ['./standing.component.css'],
 })
 export class StandingComponent implements OnInit {
+  public UsingAsync: boolean = false;
+  public MyTeam: Team[];
+  public LanguageName: string;
+  public MySchedule: Schedule[];
+  public Standings: Rangking[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _titleService: Title,
+    private _soccerService: SoccerService
+  ) {
+    this._titleService.setTitle('Pertangdingan YGY');
+    this.MyTeam = [];
+    this.MySchedule = [];
+    this.Standings = [];
+    this.getTeam();
+    this.LanguageName = 'liga';
+    this.ComputerRangking();
   }
 
+  ngOnInit(): void {}
+  getTeam() {
+    if (this.UsingAsync) {
+      let xx = this._soccerService.getTeamAsync();
+      xx.then((Teams: Team[]) => (this.MyTeam = Teams));
+    } else {
+      this.MyTeam = this._soccerService.getTeam();
+    }
+  }
+
+  private getSchdule() {
+    if (this.UsingAsync) {
+      let xx = this._soccerService.getScheduleAsync();
+      xx.then((schedules: Schedule[]) => (this.MySchedule = schedules));
+    } else {
+      this.MySchedule = this._soccerService.getSchedule();
+    }
+  }
+
+  public ComputerRangking() {
+    var curDate: Date = new Date();
+    var TeamAt: number;
+    this.Standings = [];
+    this.MySchedule.forEach((element) => {
+      if (element.PlayingDate < curDate && element.HomeScore >= 0) {
+        TeamAt = this.FindTeam(element.HomeTeam);
+        if (TeamAt < 0) {
+          this.Standings.push({
+            TeamName: element.AwayTeam,
+            GamePlayed: 0,
+            wins: 0,
+            Ties: 0,
+            GoalsFor: 0,
+            GoalAgaints: 0,
+          });
+          TeamAt = this.Standings.length - 1;
+        }
+        this.UpdCurrentRow(element, TeamAt, 'A');
+      }
+    });
+  }
+  private UpdCurrentRow(element: Schedule, TeamAt: number, HomeAway: string) {
+    this.Standings[TeamAt].GamePlayed++;
+    if (HomeAway == 'H') {
+      this.Standings[TeamAt].GoalsFor += element.HomeScore;
+      this.Standings[TeamAt].GoalAgaints += element.AwayScore;
+      // win menang
+      if (element.HomeScore > element.AwayScore) {
+        this.Standings[TeamAt].wins++;
+      }
+    }
+    if (HomeAway == 'A') {
+      this.Standings[TeamAt].GoalsFor += element.AwayScore;
+      this.Standings[TeamAt].GoalsFor += element.HomeScore;
+      if (element.AwayScore > element.HomeScore) {
+        this.Standings[TeamAt].wins++;
+      }
+    }
+    if (element.AwayScore == element.HomeScore) {
+      this.Standings[TeamAt].Ties++;
+    }
+  }
+  private FindTeam(TeamName: string): number {
+    var FoundAt: number = -1;
+    for (var _x = 0; _x < this.Standings.length; _x++) {
+      if (this.Standings[_x].TeamName == TeamName) {
+        return _x;
+      }
+    }
+    return FoundAt;
+  }
 }
